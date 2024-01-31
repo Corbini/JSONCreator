44be595bfafa55d528e94052f2b7cd8a0ae88aa9
pstrama-esmetric <patryk.strama@esmetric.pl>
2024-01-31 09:23:50 +0100

    General setting added

--- source/frame/parameter/_generals.py
+++ source/frame/parameter/_generals.py
@@ -5,29 +5,30 @@ def create_generals(self):
     self.general = Frame(
         self,
         bg="green",
-        width=150,
-        height=200
+        width=150
     )
 
-    self.general_type = Text(self.general, text='string')
-    self.general_type.insert(1,"untyped")
+
+    self.general_type = Entry(self.general, text="untyped")
+    # self.general_type.insert(1.0,"untyped")
     self.general_type.pack(side='top',fill='x', expand=True)
 
-    # self.general_en_text = Label(self.general, name="angielska nazwa")
-    # self.general_en_text.pack(side='top',fill='x', expand=True)
-    # self.general_en = Entry(self.general, name="untyped")
-    # self.general_en.pack(side='top',fill='x', expand=True)
+    self.general_en_text = Label(self.general, text="angielska nazwa")
+    self.general_en_text.pack(side='top',fill='x', expand=True)
+    self.general_en = Entry(self.general)
+    self.general_en.pack(side='top',fill='x', expand=True)
 
-    # self.general_pl_text = Label(self.general, name="polska nazwa")
-    # self.general_pl_text.pack(side='top',fill='x', expand=True)
-    # self.general_pl = Entry(self.general, name="untyped")
-    # self.general_pl.pack(side='top',fill='x', expand=True)
+    self.general_pl_text = Label(self.general, text="polska nazwa")
+    self.general_pl_text.pack(side='top',fill='x', expand=True)
 
-    hide_generals(self)
+    self.general_pl = Entry(self.general)
+    self.general_pl.pack(side='top',fill='x', expand=True)
+
+    show_generals(self)
 
 
 def show_generals(self):
-    self.general.grid_forget()
+    self.general.grid(row=1, column=0, sticky='nw')
 
 def hide_generals(self):
-    self.general.grid(row=1, column=0, sticky='nw')
+    self.general.grid_forget()
