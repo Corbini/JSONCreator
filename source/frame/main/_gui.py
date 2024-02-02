868e31facd5c95f1ac04618b84cc9d51f9259042
pstrama-esmetric <patryk.strama@esmetric.pl>
2024-02-02 13:03:58 +0100

    Fixed horizontal scroll wheel. Added vertical scroll wheel.

--- source/frame/main/_gui.py
+++ source/frame/main/_gui.py
@@ -81,60 +81,67 @@ def create_menu(self):
 
         self.tree_canvas = Canvas(self)
 
-        self.tree_canvas.pack(side='left', expand='True', fill='both', padx=10, pady=10)
+        self.tree_canvas.pack(expand='True', fill='both', padx=10, pady=10)
         
         self.tree_frame = Frame(self.tree_canvas, padx=2, pady=2, borderwidth=0, relief='flat')
 
-        self.scroll = Scrollbar(self, orient='vertical', command=self.tree_canvas.yview)
-        self.tree_canvas.configure(yscrollcommand=self.scroll.set)
+        self.scroll_horizontal = Scrollbar(self.tree_canvas, orient='horizontal', command=self.tree_canvas.xview)
+        self.tree_canvas.configure(xscrollcommand=self.scroll_horizontal.set)
+        self.scroll_horizontal_state = False
+        
+        self.scroll_vertical = Scrollbar(self.tree_canvas, orient='vertical', command=self.tree_canvas.yview)
+        self.tree_canvas.configure(yscrollcommand=self.scroll_vertical.set)
+        self.scroll_vertical_state = False
 
         self.tree_canvas.create_window((0, 0), window=self.tree_frame, anchor='nw')
         
-        self.tree_frame.bind("<Configure>", lambda event: self.scroll_show(event))
-
+        self.tree_canvas.bind("<Configure>", self.scroll_update)
+        self.tree_frame.bind("<Configure>", self.scroll_update)
 
 def on_scrollwheel(self, event):
      self.tree_canvas.yview_scroll(int(-1*(event.delta/120)), "units")
 
-
-def scroll_show(self, event):
+def scroll_update(self, event):
     if event.widget is self.tree_canvas:
         canvas_height = event.height
+        canvas_width = event.width
     else:
         self.tree_canvas.update()
         canvas_height = self.tree_canvas.winfo_height()
+        canvas_width = self.tree_canvas.winfo_width()
     
     if event.widget is self.tree_frame:
         frame_height = event.height
+        frame_width = event.width
     else:
         self.tree_frame.update()
         frame_height = self.tree_frame.winfo_height()
+        frame_width = self.tree_frame.winfo_width()
 
-    if frame_height>canvas_height - 25:
-        self.scroll.pack(side='right', fill='y')
-            
+    if self.scroll_horizontal_state:
+        if frame_width<=canvas_width - 25:
+            self.scroll_horizontal.pack_forget()
+            self.tree_canvas.xview_scroll(0, "units")
+            self.scroll_horizontal_state = False
+        else:
+            self.tree_canvas.configure(scrollregion = self.tree_canvas.bbox('all'))
+    elif frame_width>canvas_width - 25:
+        self.scroll_horizontal.pack(side='bottom', fill='x', anchor='sw')
         self.tree_canvas.configure(scrollregion = self.tree_canvas.bbox('all'))
-        self.bind_all("<MouseWheel>", self.on_scrollwheel)
-        self.tree_frame.bind("<Configure>", lambda event: self.scroll_hide(event))
+        self.scroll_horizontal_state = True
         
-def scroll_hide(self, event):
-    if event.widget is self.tree_canvas:
-        canvas_height = event.height
-    else:
-        self.tree_canvas.update()
-        canvas_height = self.tree_canvas.winfo_height()
-    
-    if event.widget is self.tree_frame:
-        frame_height = event.height
-    else:
-        self.tree_frame.update()
-        frame_height = self.tree_frame.winfo_height()
+ 
+    if self.scroll_vertical_state:
+        if frame_height<=canvas_height - 25:
+            self.scroll_vertical.pack_forget()
 
-    if frame_height<=canvas_height - 25:
-        self.scroll.pack_forget()
-
-        self.tree_canvas.unbind_all("<MouseWheel>")
-        self.tree_canvas.yview_scroll(-100, "units")
-        self.tree_frame.bind("<Configure>", lambda event: self.scroll_show(event))
-    else:
+            self.tree_canvas.unbind_all("<MouseWheel>")
+            self.tree_canvas.yview_scroll(0, "units")
+            self.scroll_vertical_state = False
+        else:
+            self.tree_canvas.configure(scrollregion = self.tree_canvas.bbox('all'))
+    elif frame_height>canvas_height - 25:
+        self.bind_all("<MouseWheel>", self.on_scrollwheel)
+        self.scroll_vertical.pack(side='right', fill='y')
         self.tree_canvas.configure(scrollregion = self.tree_canvas.bbox('all'))
+        self.scroll_vertical_state = True
