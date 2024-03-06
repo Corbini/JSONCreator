1832a20888e88fadcc9fcaab6e82d5e6ff52e27e
pstrama-esmetric <patryk.strama@esmetric.pl>
2024-03-06 15:45:30 +0100

    Added undo_redo class to converter. The app doesn't use it now. Updated todo.md

---
+++ source/model/undo_redo.py
@@ -0,0 +1,35 @@
+from copy import deepcopy
+
+
+class UndoRedo:
+    def __init__(self, maximum):
+
+        self._maximum = maximum
+        self._saves = []
+        self._position = -1
+
+    def save(self, data):
+        saved_data = deepcopy(data)
+        if self._position >= self._maximum:
+            self._saves.pop(0)
+        else:
+            self._position += 1
+            self._saves = self._saves[:self._position]
+
+        self._saves.insert(self._position, saved_data)
+
+    def undo(self):
+        if self._position < 0:
+            return None
+
+        self._position -= 1
+
+        return self._saves[self._position]
+
+    def redo(self):
+        if self._position > len(self._saves) - 1:
+            return None
+
+        self._position += 1
+
+        return self._saves[self._position]
--- todo.md
+++ todo.md
@@ -3,3 +3,4 @@ After testing with outsider i conclude:
  - Applying value by clicking enter after writing is not intuitive
  - The view is old-fashioned
  - The incorrect value is the parameter is not shown 
+ - translations for enums
